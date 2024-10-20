0ee777818659f7a5090b15436b254736bd2c6528
Samscuckoo <samuelcanellasf@gmail.com>
2024-10-20 14:42:37 -0300

    Fazendo login

--- catcare_back/src/controllers/userController.ts
+++ catcare_back/src/controllers/userController.ts
@@ -1,5 +1,9 @@
 import userService from '../services/userService';
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import {PrismaClient, User } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 class UserController {
   async register(req: Request, res: Response): Promise<void> {
@@ -13,8 +17,16 @@ class UserController {
     }
   }
 
-  async login(req: Request, res: Response) {
+  async login(req: Request, res: Response): Promise<void> {
     const{email, password} = req.body;
+
+    try {
+      const login = await userService.loginUser(email, password);
+    } catch (error:any) {
+      res.status(400).json({ error: error.message });
+    }
+
+    
   }
 }
 
--- catcare_back/src/services/userService.ts
+++ catcare_back/src/services/userService.ts
@@ -3,6 +3,11 @@ import { PrismaClient, User } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+interface body {
+  email: string;
+  password: string;
+}
+
 class UserService {
   async registerUser(email: string, name: string, password: string): Promise<User> {
     const salt = await bcrypt.genSalt(10);
@@ -23,6 +28,36 @@ class UserService {
 
     return user;
   }
+  
+  async loginUser(email: string, password: string): Promise<User | null> {
+  try{
+    //Verificar se o usuário existe
+    const user = await prisma.user.findFirst({
+      where: {email},
+      include: {credentials: true} //inclui as credenciais do usuário
+    });
+
+    if (!user) {
+      return res.status(404).json({error: 'Usuário não encontrado. Considere criar uma conta.'});
+    }
+
+    // Verifica se as credenciais existem
+    // if (!user.credentials) {
+    //   return res.status(400).json({ error: 'Credenciais não encontradas' });
+    // }
+    
+    const isMatch = await bcrypt.compare(password, user.credentials.passwordHash);
+
+    if (!isMatch) {
+      return res.status(401).json({error: 'Senha incorreta'});
+    }
+
+    // Login bem-sucedido
+    return res.status(200).json({ message: 'Login bem-sucedido', user });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+
 }
 
 export default new UserService();
