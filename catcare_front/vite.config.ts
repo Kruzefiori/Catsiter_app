2c62a48896635d00ebdf36962195ce18c06feee4
Augusto <augusto.lr06@gmail.com>
2024-10-21 17:30:26 -0300

    feat: make google login work

--- catcare_front/vite.config.ts
+++ catcare_front/vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react'
 import * as path from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
+// import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
