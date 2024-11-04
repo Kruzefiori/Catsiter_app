85c448d7e4fcfe9baa64b5e61640fbf86d5957f1
joevtap <joelvtorres2024@gmail.com>
2024-11-04 20:43:16 -0300

    test: add testing framework

---
+++ catcare_back/src/__tests__/jest.test.ts
@@ -0,0 +1,9 @@
+describe("Testing the Jest framework", () => {
+  it("should test that true is true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("should test that false is not true", () => {
+    expect(false).not.toBe(true);
+  });
+});
--- catcare_back/src/services/bookingService.ts
+++ catcare_back/src/services/bookingService.ts
@@ -1,179 +1,201 @@
-import { PrismaClient, Prisma, bookingStatus } from "@prisma/client";
-
+import { bookingStatus } from "@prisma/client";
+import prisma from "../client";
 
 interface visits {
-    visitDate: string;
-    status?: string;
-    notes?: string;
+  visitDate: string;
+  status?: string;
+  notes?: string;
 }
 
 interface bookingInfo {
-    visits: visits[];
-    requesterId: number; // user who is requesting
-    requestedId: number // user who is requested
-    startDate: string // date and time of the service starts
-    endDate: string // date and time of the service ends
-    generalNotes?: string;
+  visits: visits[];
+  requesterId: number; // user who is requesting
+  requestedId: number; // user who is requested
+  startDate: string; // date and time of the service starts
+  endDate: string; // date and time of the service ends
+  generalNotes?: string;
 }
 
 class BookingService {
-    prisma = new PrismaClient();
+  prisma = prisma;
 
-    async createBooking(body: bookingInfo) {
-        const { visits, requesterId, requestedId, startDate, endDate, generalNotes } = body;
-        // Create a booking
-        const booking = await this.prisma.booking.create({
-            data: {
-                requesterId: requesterId,
-                requestedId: requestedId,
-                startDate: (new Date(startDate)).toISOString(),
-                endDate: (new Date(endDate)).toISOString(),
-                totalVisits: visits.length,
-                generalNotes: generalNotes,
-            }
-        });
+  async createBooking(body: bookingInfo) {
+    const {
+      visits,
+      requesterId,
+      requestedId,
+      startDate,
+      endDate,
+      generalNotes,
+    } = body;
+    // Create a booking
+    const booking = await this.prisma.booking.create({
+      data: {
+        requesterId: requesterId,
+        requestedId: requestedId,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+        totalVisits: visits.length,
+        generalNotes: generalNotes,
+      },
+    });
 
-        // Create visits
-        visits.forEach(async (visit) => {
-            await this.prisma.visit.create({
-                data: {
-                    visitDate: (new Date(visit.visitDate)).toISOString(),
-                    bookingId: booking.id,
-                }
-            });
-        });
+    // Create visits
+    visits.forEach(async (visit) => {
+      await this.prisma.visit.create({
+        data: {
+          visitDate: new Date(visit.visitDate).toISOString(),
+          bookingId: booking.id,
+        },
+      });
+    });
 
-        return booking;
+    return booking;
+  }
+
+  async updateBooking(bookingId: number, body: bookingInfo) {
+    const {
+      visits,
+      requesterId,
+      requestedId,
+      startDate,
+      endDate,
+      generalNotes,
+    } = body;
+
+    const booking = await this.prisma.booking.findUnique({
+      where: { id: bookingId },
+    });
+
+    if (!booking) {
+      throw new Error("Booking not found");
     }
 
-    async updateBooking(bookingId: number, body: bookingInfo) {
-        const { visits, requesterId, requestedId, startDate, endDate, generalNotes } = body;
-
-        const booking = await this.prisma.booking.findUnique({
-            where: { id: bookingId }
-        });
-
-        if (!booking) {
-            throw new Error("Booking not found");
-        }
-
-        if (booking.status !== "PENDING") {
-            //update status to pending
-            await this.prisma.booking.update({
-                where: { id: bookingId },
-                data: { status: "PENDING" }
-            });
-        }
-
-        // Update booking
-        const update = await this.prisma.booking.update({
-            where: { id: bookingId },
-            data: {
-                requesterId: requesterId ? requesterId : booking.requesterId,
-                requestedId: requestedId ? requestedId : booking.requestedId,
-                startDate: startDate ? startDate : booking.startDate,
-                endDate: endDate ? endDate : booking.endDate,
-                totalVisits: visits.length,
-                generalNotes: generalNotes ? generalNotes : booking.generalNotes,
-            }
-        });
-
-        // Delete all visits
-        await this.prisma.visit.deleteMany({
-            where: {
-                bookingId: bookingId
-            }
-        });
-
-        // Create visits
-        visits.forEach(async (visit) => {
-            await this.prisma.visit.create({
-                data: {
-                    visitDate: visit.visitDate,
-                    bookingId: bookingId,
-                    visitNotes: visit.notes ? visit.notes : "",
-                }
-            });
-        });
-
-        return update;
+    if (booking.status !== "PENDING") {
+      //update status to pending
+      await this.prisma.booking.update({
+        where: { id: bookingId },
+        data: { status: "PENDING" },
+      });
     }
 
-    async getBookingByRequesterAndStatus(userId: number, status: bookingStatus ) {
-        return await this.prisma.booking.findMany({
-            where: {
-                requesterId: userId,
-                status: status
-            },
-        });
-    }
+    // Update booking
+    const update = await this.prisma.booking.update({
+      where: { id: bookingId },
+      data: {
+        requesterId: requesterId ? requesterId : booking.requesterId,
+        requestedId: requestedId ? requestedId : booking.requestedId,
+        startDate: startDate ? startDate : booking.startDate,
+        endDate: endDate ? endDate : booking.endDate,
+        totalVisits: visits.length,
+        generalNotes: generalNotes ? generalNotes : booking.generalNotes,
+      },
+    });
 
-    async getBookingByRequestedAndStatus(userId: number, status: bookingStatus) {
-        return await this.prisma.booking.findMany({
-            where: {
-                requestedId: userId,
-                status: status
-            },
-        });
-    }
+    // Delete all visits
+    await this.prisma.visit.deleteMany({
+      where: {
+        bookingId: bookingId,
+      },
+    });
 
-    async answerBooking(bookingId: number, status: bookingStatus) {
-        if(status === "ACCEPTED") {
-            return await this.acceptBooking(bookingId);
-        }
-        await this.rejectBooking(bookingId);
-    }
+    // Create visits
+    visits.forEach(async (visit) => {
+      await this.prisma.visit.create({
+        data: {
+          visitDate: visit.visitDate,
+          bookingId: bookingId,
+          visitNotes: visit.notes ? visit.notes : "",
+        },
+      });
+    });
 
-    async acceptBooking(bookingId: number) {
-        return await this.prisma.booking.update({
-            where: { id: bookingId },
-            data: { status: "ACCEPTED" }
-        });
-    }
+    return update;
+  }
 
-    async rejectBooking(bookingId: number) {
-        return await this.prisma.booking.update({
-            where: { id: bookingId },
-            data: { status: "REJECTED" }
-        });
-    }    
+  async getBookingByRequesterAndStatus(userId: number, status: bookingStatus) {
+    return await this.prisma.booking.findMany({
+      where: {
+        requesterId: userId,
+        status: status,
+      },
+    });
+  }
 
-    async getPendingVisitByUserId(userId: number) {
-        return await this.prisma.visit.findMany({
-            where: {
-                status: "PENDING"
-            },
-            orderBy: {
-                visitDate: 'asc'
-            }
-        });
-    }
+  async getBookingByRequestedAndStatus(userId: number, status: bookingStatus) {
+    return await this.prisma.booking.findMany({
+      where: {
+        requestedId: userId,
+        status: status,
+      },
+    });
+  }
 
-    async answerVisit(body : {visitId: number, status: string , visitNotes: string | undefined}) {
-        if(body.status === "DONE") {
-            return await this.prisma.visit.update({
-                where: { id: body.visitId },
-                data: { status: "DONE", visitNotes: body.visitNotes ? body.visitNotes : "" }
-            });
-        }
-        return await this.prisma.visit.update({
-            where: { id: body.visitId },
-            data: { status: "CANCELLED", visitNotes: body.visitNotes ? body.visitNotes : "" }
-        });
+  async answerBooking(bookingId: number, status: bookingStatus) {
+    if (status === "ACCEPTED") {
+      return await this.acceptBooking(bookingId);
     }
+    await this.rejectBooking(bookingId);
+  }
 
-    async getVisitsByBookingId(bookingId: number) {
-        return await this.prisma.visit.findMany({
-            where: {
-                bookingId: bookingId
-            },
-            include: {
-                booking: true
-            }
-        });
+  async acceptBooking(bookingId: number) {
+    return await this.prisma.booking.update({
+      where: { id: bookingId },
+      data: { status: "ACCEPTED" },
+    });
+  }
+
+  async rejectBooking(bookingId: number) {
+    return await this.prisma.booking.update({
+      where: { id: bookingId },
+      data: { status: "REJECTED" },
+    });
+  }
+
+  async getPendingVisitByUserId(userId: number) {
+    return await this.prisma.visit.findMany({
+      where: {
+        status: "PENDING",
+      },
+      orderBy: {
+        visitDate: "asc",
+      },
+    });
+  }
+
+  async answerVisit(body: {
+    visitId: number;
+    status: string;
+    visitNotes: string | undefined;
+  }) {
+    if (body.status === "DONE") {
+      return await this.prisma.visit.update({
+        where: { id: body.visitId },
+        data: {
+          status: "DONE",
+          visitNotes: body.visitNotes ? body.visitNotes : "",
+        },
+      });
     }
+    return await this.prisma.visit.update({
+      where: { id: body.visitId },
+      data: {
+        status: "CANCELLED",
+        visitNotes: body.visitNotes ? body.visitNotes : "",
+      },
+    });
+  }
+
+  async getVisitsByBookingId(bookingId: number) {
+    return await this.prisma.visit.findMany({
+      where: {
+        bookingId: bookingId,
+      },
+      include: {
+        booking: true,
+      },
+    });
+  }
 }
-
-
 
 export default new BookingService();
