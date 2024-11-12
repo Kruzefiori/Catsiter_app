7795a7485326e5ecc64814def1e06e5243a90140
Augusto <augusto.lr06@gmail.com>
2024-11-12 09:20:35 -0300

    refactor: pass google user save function to main file

--- catcare_back/src/controllers/authController.ts
+++ catcare_back/src/controllers/authController.ts
@@ -93,46 +93,29 @@ class UserController {
 			const { tokens } = await oAuth2Client.getToken(code);
 			oAuth2Client.setCredentials(tokens);
 
-			const googleUserInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
-			const { data: userProfile } = await axios.get<UserProfile>(
-				googleUserInfoUrl,
-				{
-					headers: {
-						Authorization: `Bearer ${tokens.access_token}`,
-					},
-				}
-			);
-
-			const googleUser = {
-				id: userProfile.id,
-				email: userProfile.email,
-				name: userProfile.name,
-				picture: userProfile.picture,
-			};
-
-			const signUpSchema = z.object({
-				id: z.string(),
-				email: z.string().email(),
-				name: z.string(),
-				picture: z.string(),
+			const ticket = await oAuth2Client.verifyIdToken({
+				idToken: tokens.id_token!,
+				audience: CLIENT_ID,
 			});
 
-			try {
-				signUpSchema.parse(googleUser);
-			} catch (error) {
-				if (error instanceof ZodError)
-					res.status(400).json({ error: error.errors });
+			const payload = ticket.getPayload();
+
+			if (!payload?.email || !payload?.name || !payload?.sub) {
+				res.status(400).send("Erro ao obter informações do usuário");
 				return;
 			}
 
-			// await authService.saveGoogleUser(googleUser.email, googleUser.name);
-			res.status(200).json({
-				token: tokens.access_token,
-				user: googleUser,
-			});
+			// Salvar ou atualizar o usuário no banco de dados
+			const userData = await authService.googleSignIn(
+				payload?.email,
+				payload?.name,
+				payload?.sub
+			);
+
+			res.json(userData);
 		} catch (error) {
-			console.error("An error occurred on google login:", error);
-			res.status(400).json(error);
+			console.error("Erro ao autenticar:", error);
+			res.status(500).send("Erro ao salvar usuário no banco de dados");
 		}
 	}
 }
--- catcare_back/src/services/authService.ts
+++ catcare_back/src/services/authService.ts
@@ -4,148 +4,153 @@ import jwt from "jsonwebtoken";
 import { Prisma } from "@prisma/client";
 
 class authService {
-  prisma = prisma;
+	prisma = prisma;
 
-  async signUp(email: string, name: string, password: string) {
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+	async signUp(email: string, name: string, password: string) {
+		const salt = await bcrypt.genSalt(10);
+		const passwordHash = await bcrypt.hash(password, salt);
 
-    try {
-      const existingUser = await this.prisma.user.findFirst({
-        where: { email },
-        include: { UserCredentials: true },
-      });
+		try {
+			const existingUser = await this.prisma.user.findFirst({
+				where: { email },
+				include: { UserCredentials: true },
+			});
 
-      if (existingUser) {
-        // Verificar se o usuário pode atualizar suas credenciais
-        if (!existingUser.googleId || existingUser.UserCredentials.length > 0) {
-          throw new Error("email already exists");
-        }
+			if (existingUser) {
+				// Verificar se o usuário pode atualizar suas credenciais
+				if (!existingUser.googleId || existingUser.UserCredentials.length > 0) {
+					throw new Error("email already exists");
+				}
 
-        // Atualizar o usuário existente com as novas credenciais
-        await this.prisma.user.update({
-          where: { email },
-          data: {
-            name,
-            UserCredentials: {
-              upsert: {
-                where: { userId: existingUser.id },
-                create: {
-                  passwordHash,
-                  salt,
-                },
-                update: {
-                  passwordHash,
-                  salt,
-                },
-              },
-            },
-          },
-        });
-      } else {
-        // Criar um novo usuário
-        await this.prisma.user.create({
-          data: {
-            email,
-            name,
-            UserCredentials: {
-              create: {
-                passwordHash,
-                salt,
-              },
-            },
-          },
-        });
-      }
+				// Atualizar o usuário existente com as novas credenciais
+				await this.prisma.user.update({
+					where: { email },
+					data: {
+						name,
+						UserCredentials: {
+							upsert: {
+								where: { userId: existingUser.id },
+								create: {
+									passwordHash,
+									salt,
+								},
+								update: {
+									passwordHash,
+									salt,
+								},
+							},
+						},
+					},
+				});
+			} else {
+				// Criar um novo usuário
+				await this.prisma.user.create({
+					data: {
+						email,
+						name,
+						UserCredentials: {
+							create: {
+								passwordHash,
+								salt,
+							},
+						},
+					},
+				});
+			}
 
-      return;
-    } catch (err) {
-      if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        if (err.code === "P2002") {
-          throw new Error("email already exists");
-        }
-      }
+			return;
+		} catch (err) {
+			if (err instanceof Prisma.PrismaClientKnownRequestError) {
+				if (err.code === "P2002") {
+					throw new Error("email already exists");
+				}
+			}
 
-      throw err;
-    }
-  }
+			throw err;
+		}
+	}
 
-  async signIn(email: string, password: string) {
-    try {
-      const user = await this.prisma.user.findFirst({
-        where: { email },
-        include: { UserCredentials: true },
-      });
+	async signIn(email: string, password: string) {
+		try {
+			const user = await this.prisma.user.findFirst({
+				where: { email },
+				include: { UserCredentials: true },
+			});
 
-      if (!user) {
-        throw new Error("user not found");
-      }
+			if (!user) {
+				throw new Error("user not found");
+			}
 
-      if (user.UserCredentials.length === 0) {
-        throw new Error("please sign in with your google account");
-      }
+			if (user.UserCredentials.length === 0) {
+				throw new Error("please sign in with your google account");
+			}
 
-      const { passwordHash } = user.UserCredentials[0];
+			const { passwordHash } = user.UserCredentials[0];
 
-      const isMatch = await bcrypt.compare(password, passwordHash);
+			const isMatch = await bcrypt.compare(password, passwordHash);
 
-      if (!isMatch) {
-        throw new Error("password incorrect");
-      }
+			if (!isMatch) {
+				throw new Error("password incorrect");
+			}
 
-      if (!process.env.JWT_SECRET) {
-        throw new Error("JWT_SECRET not defined");
-      }
+			if (!process.env.JWT_SECRET) {
+				throw new Error("JWT_SECRET not defined");
+			}
 
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-        issuer: "catcare",
-        expiresIn: "7d",
-        algorithm: "HS256",
-      });
+			const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+				issuer: "catcare",
+				expiresIn: "7d",
+				algorithm: "HS256",
+			});
 
-      return token;
-    } catch (error) {
-      throw error;
-    }
-  }
+			return token;
+		} catch (error) {
+			throw error;
+		}
+	}
 
-  async googleSignIn(email: string, name: string, googleId: string) {
-    try {
-      let user = await this.prisma.user.findFirst({
-        where: { email },
-      });
+	async googleSignIn(email: string, name: string, googleId: string) {
+		try {
+			let user = await this.prisma.user.findFirst({
+				where: { email },
+			});
 
-      if (!user) {
-        user = await this.prisma.user.create({
-          data: {
-            email,
-            name,
-            googleId,
-          },
-        });
-      } else {
-        // Atualizar o googleId se o usuário já existir
-        user = await this.prisma.user.update({
-          where: { email },
-          data: { googleId },
-        });
-      }
+			if (!user) {
+				user = await this.prisma.user.create({
+					data: {
+						email,
+						name,
+						googleId,
+					},
+				});
+			} else {
+				// Atualizar o googleId se o usuário já existir
+				user = await this.prisma.user.update({
+					where: { email },
+					data: { googleId },
+				});
+			}
 
-      if (!process.env.JWT_SECRET) {
-        throw new Error("JWT_SECRET not defined");
-      }
+			if (!process.env.JWT_SECRET) {
+				throw new Error("JWT_SECRET not defined");
+			}
 
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-        issuer: "catcare",
-        expiresIn: "7d",
-        algorithm: "HS256",
-      });
+			const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+				issuer: "catcare",
+				expiresIn: "7d",
+				algorithm: "HS256",
+			});
 
-      return token;
-    } catch (error) {
-      throw new Error("Erro ao salvar usuário no banco de dados");
-    }
-  }
+			return {
+				token,
+				id: user.id,
+				name: user.name,
+				email: user.email,
+			};
+		} catch (error) {
+			throw new Error("Erro ao salvar usuário no banco de dados");
+		}
+	}
 }
 
 export default new authService();
